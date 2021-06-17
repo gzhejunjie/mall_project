698bd8b67e48d2004beed48e88ad9e1e71c81ee2
JackyinGitHub <877143789@qq.com>
2021-06-17 20:57:42 +0800

    nav-bar

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot  name="left"></slot></div>
+    <div class="center"><slot  name="center"></slot></div>
+    <div class="right"><slot  name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"NavBar"
+  }
+</script>
+
+<style scoped>
+  .nav-bar {
+    display: flex;
+    height:44px;
+    line-height: 44px;
+  }
+
+  .left {
+    width: 60px;
+    /* background-color: blue; */
+  }
+
+  .center {
+    flex: 1;
+    text-align: center;
+    /* background-color: red; */
+  }
+
+  .right {
+    width: 60px;
+    /* background-color: blue; */
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,24 @@
 <template>
-  <div>
-    <h2>首页</h2>
-
+  <div id="home">
+    <nav-bar class="navbar"><div slot="center">购物车</div></nav-bar>
   </div>
 </template>
 
 <script>
+  import NavBar from 'components/common/navbar/NavBar'
+
   export default {
-    name:"Home"
+    name:"Home",
+    components: {
+      NavBar
+    }
   }
 </script>
 
 <style scoped>
-
+  .navbar {
+    background-color: var(--color-tint);
+    color: #fff;
+    box-shadow: 0px 1px 1px rgba(100, 100, 100, .2);
+  }
 </style>
